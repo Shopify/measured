1546428696c271e08f57c40ad1c3a21f13fa4e24
Kirin Rastogi <rastogikirin@gmail.com>
2017-04-25 10:14:59 -0400

    Add si_unit helper and tests (#90)
    Adds a helper to the unit builder DSL which creates a unit with all of the SI prefixes – kilo (k), Mega (M), micro (μ), etc.

--- lib/measured/unit_system_builder.rb
+++ lib/measured/unit_system_builder.rb
@@ -8,11 +8,48 @@ class Measured::UnitSystemBuilder
     nil
   end
 
+  def si_unit(unit_name, aliases: [], value: nil)
+    @units += build_si_units(unit_name, aliases: aliases, value: value)
+    nil
+  end
+
   def build
     Measured::UnitSystem.new(@units)
   end
 
   private
+
+  SI_PREFIXES = [
+    ["y", "yocto", -24],
+    ["z", "zepto", -21],
+    ["a", "atto", -18],
+    ["f", "femto", -15],
+    ["p", "pico", -12],
+    ["n", "nano", -9],
+    ["μ", "micro", -6],
+    ["m", "milli", -3],
+    ["c", "centi", -2],
+    ["d", "deci", -1],
+    ["da", "deca", 1],
+    ["h", "hecto", 2],
+    ["k", "kilo", 3],
+    ["M", "mega", 6],
+    ["G", "giga", 9],
+    ["T", "tera", 12],
+    ["P", "peta", 15],
+    ["E", "exa", 18],
+    ["Z", "zetta", 21],
+    ["Y", "yotta", 24]
+  ]
+
+  def build_si_units(name, aliases: [], value: nil)
+    si_units = [build_unit(name, aliases: aliases, value: value)]
+    SI_PREFIXES.each do |short, long, exp|
+      long_names = aliases.map { |suffix| "#{long}#{suffix}" }
+      si_units << build_unit("#{short}#{name}", aliases: long_names, value: "#{10 ** exp} #{name}")
+    end
+    si_units
+  end
 
   def build_unit(name, aliases: [], value: nil)
     unit = Measured::Unit.new(name, aliases: aliases, value: value)
--- test/unit_system_builder_test.rb
+++ test/unit_system_builder_test.rb
@@ -37,4 +37,76 @@ class Measured::UnitSystemBuilderTest < ActiveSupport::TestCase
 
     assert_equal 'BOLD', measurable.unit_system.unit_for!(:BOLD).name
   end
+
+  test "#si_unit adds 21 new units" do
+    measurable = Measured.build do
+      unit :ft
+      si_unit :in, value: "12 ft", aliases: [:ins]
+    end
+
+    assert_equal 22, measurable.unit_names.count
+  end
+
+  test "#si_unit creates units with si prefixes" do
+    measurable = Measured.build do
+      unit :in
+      si_unit :ft, value: "12 in", aliases: [:fts]
+    end
+
+    prefixes = Measured::UnitSystemBuilder::SI_PREFIXES.map { |short, _, _| "#{short}ft" }
+    prefixes += %w(in ft) 
+    assert_equal prefixes.sort, measurable.unit_names
+  end
+
+  test "#si_unit cannot add duplicate unit names" do
+    assert_raises Measured::UnitError do
+      Measured.build do
+        unit :m
+        si_unit :in, aliases: [:inch], value: "0.0254 m"
+        si_unit :in, aliases: [:thing], value: "123 m"
+      end
+    end
+
+    assert_raises Measured::UnitError do
+      Measured.build do
+        unit :m
+        si_unit :in, aliases: [:inch], value: "0.0254 m"
+        si_unit :inch, aliases: [:thing], value: "123 m"
+      end
+    end
+  end
+
+  test "#si_unit is case sensitive" do
+    measurable = Measured.build do
+      unit :normal
+      si_unit :bold, value: "10 normal"
+      si_unit :BOLD, value: "100 normal"
+    end
+
+    assert_equal "yBOLD", measurable.unit_system.unit_for!(:yBOLD).name
+    assert_equal "YBOLD", measurable.unit_system.unit_for!(:YBOLD).name
+    assert_equal "BOLD", measurable.unit_system.unit_for!(:BOLD).name
+  end
+
+  test "#si_unit generates working units when given base unit" do
+    measurable = Measured.build do
+      si_unit :g, aliases: [:gram, :grams]
+      unit :bigg, value: "1000 g"
+    end
+
+    assert_equal 1000, measurable.unit_system.unit_for!(:bigg).conversion_amount
+    assert_equal "g", measurable.unit_system.unit_for!(:bigg).conversion_unit
+    assert_equal 1000, measurable.unit_system.unit_for!(:kg).conversion_amount
+    assert_equal "g", measurable.unit_system.unit_for!(:kg).conversion_unit
+  end
+
+  test "#si_unit generates working units when given non base unit" do
+    measurable = Measured.build do
+      unit :lb
+      si_unit :long_ton, value: "2240 lb", aliases: ["long ton", "long tons"]
+    end
+
+    assert_equal (2240), measurable.unit_system.unit_for!(:long_ton).conversion_amount
+    assert_equal "lb", measurable.unit_system.unit_for!(:long_ton).conversion_unit
+  end
 end
