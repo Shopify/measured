0793ec17636a65f1be469965f13d73b5deab8b20
Kevin McPhillips <github@kevinmcphillips.ca>
2018-02-11 19:49:49 -0500

    WIP subclass in tests

--- test/cache_consistency_test.rb
+++ test/cache_consistency_test.rb
@@ -1,12 +1,10 @@
 require "test_helper"
 
 class Measured::CacheConsistencyTest < ActiveSupport::TestCase
-  test "any cache files match what would be generated without the cache" do
-    skip
-
-    Measured::Measurable.subclasses.each do |measurable|
-      if measurable.unit_system.cached?
-        # TODO
+  measurable_subclasses.each do |measurable|
+    if measurable.unit_system.cached?
+      test "cached measurable #{ measurable } is not out of sync with the definition" do
+        skip
       end
     end
   end
