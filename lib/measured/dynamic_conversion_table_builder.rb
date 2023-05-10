d1366cd905eec909de4f54e124eb24c974389595
Alexandre Barret <barret.alx@gmail.com>
2023-05-11 08:46:45 +1200

    Build dynamic conversion table

---
+++ lib/measured/dynamic_conversion_table_builder.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+module Measured
+  class DynamicConversionTableBuilder < ConversionTableBuilder
+    SELF_CONVERSION = ->(x) { x * Rational(1, 1) }
+
+    private
+
+    def after_initialize
+      @units.map!(&:to_dynamic)
+    end
+
+    def generate_table
+      validate_no_cycles
+
+      units.map(&:name).each_with_object({}) do |to_unit, table|
+        to_table = {to_unit => SELF_CONVERSION}
+
+        table.each do |from_unit, from_table|
+          conversion, inverse_conversion = find_conversion(to: from_unit, from: to_unit)
+          to_table[from_unit] = conversion
+          from_table[to_unit] = inverse_conversion
+        end
+
+        table[to_unit] = to_table
+      end
+    end
+
+    def find_direct_conversion(to:, from:)
+      units.each do |unit|
+        if unit.name == from && unit.conversion_unit == to
+          return [unit.conversion_amount, unit.inverse_conversion_amount]
+        end
+
+        if unit.name == to && unit.conversion_unit == from
+          return [unit.inverse_conversion_amount, unit.conversion_amount]
+        end
+      end
+
+      nil
+    end
+
+    def find_tree_traversal_conversion(to:, from:)
+      traverse(from: from, to: to, units_remaining: units.map(&:name), amount: ->(x) { x }, inverse_amount: ->(x) { x })
+    end
+
+    def traverse(from:, to:, units_remaining:, amount:, inverse_amount:)
+      units_remaining = units_remaining - [from]
+
+      units_remaining.each do |name|
+        conversion, inverse_conversion = find_direct_conversion_cached(from: from, to: name)
+
+        if conversion
+          new_amount = ->(x) { conversion.call amount.call(x) }
+          new_inverse_amount = ->(x) { inverse_amount.call inverse_conversion.call(x) }
+
+          if name == to
+            return [new_amount, new_inverse_amount]
+          else
+            result = traverse(from: name, to: to, units_remaining: units_remaining, amount: new_amount, inverse_amount: new_inverse_amount)
+            return result if result
+          end
+        end
+      end
+
+      nil
+    end
+  end
+end
---
+++ test/dynamic_conversion_table_builder_test.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+require "test_helper"
+
+class Measured::DynamicConversionTableBuilderTest < ActiveSupport::TestCase
+  test "#to_h returns expected nested hashes in an indrect path" do
+    conversion_table = Measured::DynamicConversionTableBuilder.new([
+      Measured::Unit.new(:mm),
+      Measured::Unit.new(:cm, value: [
+        {
+          conversion: ->(cm) { Rational(10,1) * cm },
+          reverse_conversion: ->(mm) { mm * Rational(1,10) },
+          description: '10 mm',
+        },
+        "mm"
+      ]),
+      Measured::Unit.new(:dm, value: "10 cm"),
+      Measured::Unit.new(:m, value: "10 dm"),
+    ]).to_h
+
+    [
+      ["m", "m", Rational(1,1)],
+      ["m", "dm", Rational(10,1)],
+      ["m", "cm", Rational(100,1)],
+      ["m", "mm", Rational(1000,1)],
+      ["dm", "m", Rational(1,10)],
+      ["dm", "dm", Rational(1,1)],
+      ["dm", "cm", Rational(10,1)],
+      ["dm", "mm", Rational(100,1)],
+      ["cm", "m", Rational(1,100)],
+      ["cm", "dm", Rational(1,10)],
+      ["cm", "cm", Rational(1,1)],
+      ["cm", "mm", Rational(10,1)],
+      ["mm", "m", Rational(1,1000)],
+      ["mm", "dm", Rational(1,100)],
+      ["mm", "cm", Rational(1,10)],
+      ["mm", "mm", Rational(1,1)]
+    ].each do |(to, from, ratio)|
+      assert conversion_table[to][from].is_a?(Proc)
+      assert_equal ratio, conversion_table[to][from].call(1)
+    end
+  end
+end
